03f3dc3a270c5df23177cdb40f02c1d399215ecd
Nishkal <kashyapnishkal@gmail.com>
2019-05-18 09:46:22 +0530

    added bgdClouds component

---
+++ .vuepress/components/BgDClouds.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="parent-container">
+    <div class="item">
+      <BgClouds :color1="color1" :color2="color2" :divisions="divisions" :start="start" :end="end"/>
+    </div>
+    <div class="item reverse">
+      <BgClouds :color1="color1" :color2="color2" :divisions="divisions" :start="start" :end="end"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["color1", "color2", "divisions", "start", "end"]
+};
+</script>
+
+<style lang="scss" scoped>
+.parent-container {
+  height: 100%;
+  display: flex;
+
+  .item {
+    width: 50%;
+    height: 100%;
+    display: inline-block;
+    position: relative;
+  }
+
+  .reverse {
+    transform: scale(-1, -1);
+    left: -1px;
+  }
+}
+</style>
